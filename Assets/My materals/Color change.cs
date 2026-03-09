c0a6e75d0211f820c0d17b31ce4a52929cbca103
Tking <tuffdodge86@gmail.com>
2026-03-09 10:46:21 -0500

    add another ball and some other stuff

---
+++ Assets/My materals/Color change.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    public Material[] colors;
+    private MeshRenderer myRenderer;
+
+    private void Start()
+    {
+        myRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Domino") || collision.gameObject.CompareTag("Player"))
+        {
+            int rand = Random.Range(0, colors.Length);
+            myRenderer.material = colors[rand];
+        }
+    }
+}
+
